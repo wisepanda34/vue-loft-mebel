c719920e825d8e5e52877a9a560b1c03f7090d18
tylkeen1803 <evghen.naydenko@gmail.com>
2023-05-20 19:32:54 +0300

    swiper works

---
+++ src/router/router.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Main from "@/pages/Main.vue";
+import About from "@/pages/About.vue";
+import Contacts from "@/pages/Contacts.vue";
+import Account from "@/pages/Account.vue";
+import CartPage from "@/pages/Cart.vue";
+import CartProduct from "@/components/CartProduct.vue";
+import Catalog from "@/pages/Catalog.vue";
+import CardPage from "@/pages/CardPage.vue";
+
+const routes = [
+  {
+    path: '/',
+    component: Main
+  },
+  {
+  path: '/about',
+    component: About
+},
+  {
+    path: '/contacts',
+    component: Contacts
+  },
+  {
+    path: '/account',
+    component: Account
+  },
+  {
+    path: '/cart',
+    component: CartPage
+  },
+  {
+    path: '/cart/:id',
+    component: CartProduct
+  },
+  {
+    path: '/catalog',
+    component: Catalog
+  },
+  {
+    path: '/cardPage',
+    component: CardPage
+  },
+
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
